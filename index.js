bfebbad83755f8fd87a8b45165392784a6f8cbff
sovrnbenny <175053539+sovrnbenny@users.noreply.github.com>
2026-01-03 17:42:39 -0500

    Updated javascript
    Figured out why the if and else if statements weren't working. Corrected and working.

--- index.js
+++ index.js
@@ -9,7 +9,35 @@ let randomNum3;
 
 function getComputerChoice(){
     randomNum1 = Math.floor(Math.random() * 3) + 1;
-    hand1.textContent = randomNum1;
+    randomNum2 = Math.floor(Math.random() * 3) + 1;
+    randomNum3 = Math.floor(Math.random() * 3) + 1;
+
+        if(randomNum1 == 1){
+        hand1.textContent = "rock";
+        }
+        else if(randomNum1 == 2){
+        hand1.textContent = "paper"; 
+        }
+        else{hand1.textContent = "scissors";
+        }
+
+        if (randomNum2 === 1) {
+        hand2.textContent = "rock";
+        } 
+        else if (randomNum2 === 2) {
+        hand2.textContent = "paper"; 
+        } 
+        else {hand2.textContent = "scissors";
+        }
+
+        if (randomNum3 === 1) {
+        hand3.textContent = "rock";
+        } 
+        else if (randomNum3 === 2) {
+        hand3.textContent = "paper"; 
+        } 
+        else {hand3.textContent = "scissors";
+        }
 }
 
 myButton.onclick = getComputerChoice
