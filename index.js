022a82a5a8eaf464787385a4b648c90d9d0a4203
sovrnbenny <175053539+sovrnbenny@users.noreply.github.com>
2026-01-04 14:51:30 -0500

    Updated index and Java for Human Choice
    Updated some variable labeling, and figured out how to make the prompt() method to work

--- index.js
+++ index.js
@@ -1,46 +1,35 @@
 const myButton = document.getElementById('myButton');
-const hand1 = document.getElementById('hand1');
-const hand2 = document.getElementById('hand2');
-const hand3 = document.getElementById('hand3');
+const computerHand = document.getElementById('computerHand');
+const humanHand = document.getElementById('humanHand');
 let randomNum1;
-let randomNum2;
-let randomNum3;
+let randomHand;
+
+
 
 
 function getComputerChoice(){
     randomNum1 = Math.floor(Math.random() * 3) + 1;
-    randomNum2 = Math.floor(Math.random() * 3) + 1;
-    randomNum3 = Math.floor(Math.random() * 3) + 1;
 
         if(randomNum1 == 1){
-        hand1.textContent = "rock";
+        computerHand.textContent = "rock";
         }
         else if(randomNum1 == 2){
-        hand1.textContent = "paper"; 
+        computerHand.textContent = "paper"; 
         }
-        else{hand1.textContent = "scissors";
-        }
-
-        if (randomNum2 === 1) {
-        hand2.textContent = "rock";
-        } 
-        else if (randomNum2 === 2) {
-        hand2.textContent = "paper"; 
-        } 
-        else {hand2.textContent = "scissors";
-        }
-
-        if (randomNum3 === 1) {
-        hand3.textContent = "rock";
-        } 
-        else if (randomNum3 === 2) {
-        hand3.textContent = "paper"; 
-        } 
-        else {hand3.textContent = "scissors";
+        else{computerHand.textContent = "scissors";
         }
 }
-
 myButton.onclick = getComputerChoice
+
+function getHumanChoice(){
+    
+    randomHand = prompt("Choose rock, paper, or scissors");
+    humanHand.textContent = randomHand
+
+
+}
+
+myButton.onclick = getHumanChoice
 
 
 
